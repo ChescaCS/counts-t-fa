db0df926a902c997380a7a6b3149fd89b6033e8c
Francesca K <knudsenfc@gmail.com>
2019-10-17 15:30:20 -0400

    Python prompt - instructions

---
+++ counts.py
@@ -0,0 +1,67 @@
+# Instructions:
+#
+#   Implement each of the functions below to satisfy the problem, considering
+#   the following:
+#
+#     1. does your solution work?
+#     2. is your code clear and concise? is it readable?
+#     3. is your code performant?
+#
+#   When you are done, upload your code to Github and send a link to
+#   connor@fishtownanalytics.com.
+
+
+def process(i):
+    """
+    process() takes a single integer as an argument and does
+    some background processing on it.
+    process() should execute in constant time.
+    """
+    #pass
+     
+   
+def min():
+    """
+    min() returns the minimum of all of the integers passed
+    to process() so far.
+    process() should execute in constant time.
+    """
+    #pass
+
+
+def avg():
+    """
+    avg() returns the average of all of the integers passed
+    to process() so far.
+    avg() should execute in constant time.
+    """
+    #pass
+
+
+def mode():
+    """
+    mode() returns the mode (most frequently occurring) of
+    all of the integers passed to process() so far.
+    what's the execution time of mode? can mode() run in constant time?
+    """
+    #pass
+
+
+def median():
+    """
+    median() returns the median (50th percentile) of
+    all of the integers passed to process() so far.
+    what's the big-O execution time of median()?
+    """
+    #pass
+
+
+process(1)
+process(2)
+process(2)
+process(4)
+
+print(min())  # should print "1"
+print(avg())  # should print "2.25"
+print(mode())  # should print "2"
+print(median())  # should print "2"
